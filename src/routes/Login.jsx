6050e850d79cfe444c280a6774f909c6d51292f2
jhossuan <jhossuacamposd86@gmail.com>
2022-05-19 22:43:29 -0500

    validaciones con react-hook-form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,16 +1,18 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
+import { useForm } from "react-hook-form"
 import { NavLink, useNavigate } from "react-router-dom"
+import FormError from "../components/FormError"
 import { UserContext } from "../context/UserProvider"
+import { erroresFirebase } from "../utils/erroresFirebase"
 
 const Login = () => {
 
-  const {setUser, loginUser, setActive} = useContext(UserContext)
-  const [email, setEmail] = useState('prueba@test.com')
-  const [password, setPassword] = useState('prueba')
+  const {loginUser, setActive} = useContext(UserContext)
+  const { register, handleSubmit, setError, formState: { errors } } = useForm()
 
   const navigate = useNavigate()
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+
+  const onSubmit = async({email, password}) => {
     try {
       await loginUser(email, password)
       console.log('Sesion iniciada correctamente...')
@@ -18,20 +20,50 @@ const Login = () => {
       setActive(true)
     } catch (error) {
       console.log(error.code)
-      console.log('Usuario no encontrado...')
+      setError('firebase',{
+        message: erroresFirebase(error.code)
+      })
     }
   }
 
   return (
-  <div className="containerLogin">  
-      <form onSubmit={handleSubmit}>
-        <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Enter your email..."/>
-        <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Enter your password..."/>
+    <div className="containerLogin">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormError error={errors.firebase}/>
+
+        <input
+          type="email"
+          placeholder="Enter your email..."
+          {...register('email',{
+            required:{
+              value:true,
+              message: 'Obligatory field'
+            }
+          })}
+        />
+        <FormError error={errors.email}/>
+
+        <input
+          type="password"
+          placeholder="Enter your password..."
+          {...register('password',{
+            required: {
+              value: true,
+              message: 'Enter your password'
+            }
+          })}
+        />
+        <FormError error={errors.password}/>
+
         <button type="submit">Login</button>
+
       </form>
-      <small>No tienes una cuenta? Registrate <NavLink to='/register'>Aqui</NavLink></small>
-  </div>
-  )
+      <small>
+        No tienes una cuenta? Registrate <NavLink to="/register">Aqui</NavLink>
+      </small>
+    </div>
+  );
 }
 
 export default Login
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,32 +1,86 @@
 import { useContext, useState } from "react"
 import { UserContext } from "../context/UserProvider"
 import { useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { erroresFirebase } from "../utils/erroresFirebase"
+import FormError from "../components/FormError"
 
 const Register = () => {
   const { registerUser } = useContext(UserContext)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const navigate = useNavigate()
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+  const { register, handleSubmit, setError, getValues, formState: { errors } } = useForm()
+
+  const onSubmit = async({email, password}) => {
     try {
       await registerUser(email, password)
       console.log('Usuario creado correctamente', email,password)
       navigate('/login')
     } catch (error) {
       console.log(error.code)
-      console.log('Este usuario ya se encuentra registrado...')
+      setError('firebase',{
+        message: erroresFirebase(error.code)
+      })
     }
   }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input onChange={(e)=> setEmail(e.target.value) } type="text" placeholder="Enter your email..."/>
-      <input onChange={(e)=> setPassword(e.target.value)} type="password" placeholder="Enter your password..."/>
-      {/* <input onChange={(e)=> setPassword(e.target.value)} type="password" placeholder="Repeat your password..."/> */}
+  <div>
+    <h2>Register</h2>
+    <FormError error={errors.firebase}/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Enter your email..."
+        {...register('email',{
+          required: {
+            value: true,
+            message: 'Obligatory field'
+          },
+          pattern: {
+            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: 'Formato de email incorrecto'
+          }
+        })}
+      />
+      <FormError error={errors.email}/>
+
+      <input
+        type="password"
+        placeholder="Enter your password..."
+        {...register('password',{
+          required: {
+            value: true,
+          },
+          minLength: {
+            value: 6,
+            message: 'Min 6 characters'
+          },
+        })}
+      />
+      <FormError error={errors.password}/>
+
+      <input
+        type="password"
+        placeholder="Repeat your password..."
+        {...register('repassword',{
+          required:{
+            value: true,
+          },
+          validate: {
+              equals: (value) => {
+                const { password } = getValues();
+                return password === value || 'Password should match'
+              }
+          }
+        })}
+      />
+      <FormError error={errors.repassword}/>
+
       <button type="submit">Register</button>
     </form>
-  )
+  </div>
+  );
 }
 
 export default Register
